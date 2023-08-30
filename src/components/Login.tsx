0f9ac8b64d32191c412b12f2bd470ba574518d53
Nyllaah <nylla.g@hotmail.com>
2023-08-30 14:30:49 -0300

    feat: name input functional

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,16 +6,33 @@ export default function Login() {
   const [name, setName] = useState('');
   const [disabled, setDisabled] = useState(true);
 
-  function handleSubmit(e) {
+  // por que o estado name nao funciona nesse caso?
+  //   function validation() {
+  //     if (name.length >= 3) {
+  //       setDisabled(false);
+  //     }
+  //   }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = e.target;
+    setName(value);
+    return value.length >= 3 ? setDisabled(false) : setDisabled(true);
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLElement>) {
     e.preventDefault();
-    if (name.length >= 3) {
-      setDisabled(false);
-      navigate('/search');
-    }
+    setName('');
+    navigate('/search');
   }
   return (
-    <form onSubmit={ handleSubmit }>
-      <input type="text" data-testid="login-name-input" placeholder="Nome" />
+    <form onSubmit={ handleSubmit }/* onChange={ validation } */>
+      <input
+        type="text"
+        data-testid="login-name-input"
+        placeholder="Nome"
+        onChange={ handleChange }
+        value={ name }
+      />
       <button
         type="submit"
         data-testid="login-submit-button"
