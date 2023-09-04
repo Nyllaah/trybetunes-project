775821d4b60809a0e009272a53acaafc94d2e00e
Nyllaah <nylla.g@hotmail.com>
2023-09-04 18:26:32 -0300

    feat: req 10 not passing

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
+import { UserType } from '../types';
+
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [user, setUser] = useState<UserType>();
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      // setIsLoading(true);
+      const userInfo = await getUser();
+      setUser(userInfo);
+      setIsLoading(false);
+    };
+
+    getUserInfo();
+  }, []);
+
+  if (isLoading) return <Loading />;
+
+  return (
+    <>
+      <h1>Profile</h1>
+      <img src={ user.image } alt="profile" data-testid="profile-image" />
+      <div>
+        <p>{user.name}</p>
+      </div>
+      <div>
+        <p>{user.email}</p>
+      </div>
+      <div>
+        <p>{user.description}</p>
+      </div>
+      <Link to="/profile/edit">Editar perfil</Link>
+    </>
+
+  );
 }
--- src/components/ProfileEdit.tsx
+++ src/components/ProfileEdit.tsx
@@ -1,3 +1,38 @@
+import { useState, useEffect } from 'react';
+import { getUser } from '../services/userAPI';
+import { UserType } from '../types';
+import Loading from './Loading';
+
 export default function ProfileEdit() {
-  return <h1>Profile Edit</h1>;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [user, setUser] = useState<UserType>();
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      setIsLoading(true);
+      const userInfo = await getUser();
+      setUser(userInfo);
+      setIsLoading(false);
+    };
+
+    getUserInfo();
+  }, []);
+
+  if (isLoading) return <Loading />;
+
+  return (
+    <>
+      <h1>Profile Edit</h1>
+      <form action="">
+        <input type="text" placeholder={ user?.name } data-testid="edit-input-name" />
+        <input type="text" placeholder={ user?.email } data-testid="edit-input-email" />
+        <input
+          type="text"
+          placeholder={ user?.description }
+          data-testid="edit-input-description"
+        />
+        <input type="text" placeholder={ user?.image } data-testid="edit-input-image" />
+      </form>
+    </>
+  );
 }
