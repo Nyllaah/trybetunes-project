aff7a76ec63249021ad884f16c903fdd8d2d12d3
Nyllaah <nylla.g@hotmail.com>
2023-09-04 21:11:28 -0300

    feat: everything is done

--- src/components/ProfileEdit.tsx
+++ src/components/ProfileEdit.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import { getUser } from '../services/userAPI';
+import { useNavigate } from 'react-router-dom';
+import { getUser, updateUser } from '../services/userAPI';
 import { UserType } from '../types';
 import Loading from './Loading';
 
@@ -12,9 +13,29 @@ export default function ProfileEdit() {
     image: '',
   });
 
-  const handleChange = (e) => {
-    const { value } = e.target;
-  };
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  }
+
+  async function handleSubmit(e: React.FormEvent<HTMLElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+    await updateUser(user);
+    setIsLoading(false);
+  }
+
+  const isEmailValid = user.email.length !== 0 && (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+
+  function isFormValid() {
+    return isEmailValid
+    && (user.name.length !== 0)
+    && (user.description.length !== 0)
+    && (user.image.length !== 0);
+  }
 
   useEffect(() => {
     const getUserInfo = async () => {
@@ -27,31 +48,54 @@ export default function ProfileEdit() {
     getUserInfo();
   }, []);
 
+  const navigate = useNavigate();
+
   if (isLoading) return <Loading />;
 
   return (
     <>
       <h1>Profile Edit</h1>
-      <form action="">
+      <form onSubmit={ handleSubmit }>
         <input
           type="text"
           onChange={ handleChange }
           value={ user.name }
+          name="name"
+          placeholder="Nome"
           data-testid="edit-input-name"
         />
         <input
           type="text"
           onChange={ handleChange }
           value={ user.email }
+          name="email"
+          placeholder="Email"
           data-testid="edit-input-email"
         />
         <input
           type="text"
           onChange={ handleChange }
-          placeholder={ user.description }
+          value={ user.description }
+          name="description"
+          placeholder="Descrição"
           data-testid="edit-input-description"
         />
-        <input type="text" value={ user.image } data-testid="edit-input-image" />
+        <input
+          type="text"
+          onChange={ handleChange }
+          value={ user.image }
+          name="image"
+          placeholder="Imagem"
+          data-testid="edit-input-image"
+        />
+        <button
+          data-testid="edit-button-save"
+          disabled={ !isFormValid() }
+          onClick={ () => navigate('/profile') }
+        >
+          Salvar
+
+        </button>
       </form>
     </>
   );
